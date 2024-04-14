475ba30a5ce687d88ddefc053be9b4f8a2a258ee
Nguyễn Ngọc Huy <elvizhuy@gmail.com>
2024-04-14 22:46:22 +0700

    .

--- AWS/IAM/deploy.tf
+++ AWS/IAM/deploy.tf
@@ -48,7 +48,7 @@ data "aws_iam_group" "terra-group" {
 resource "aws_iam_policy_attachment" "full-access-policy-attachment" {
   for_each = aws_iam_group.terra-group
   name       = "${each.key}-full-access-policy-attachment"
-  policy_arn = aws_iam_group_policy.terra-group_policy[each.key].arn
+  policy_arn = aws_iam_policy_attachment.full-access-policy[each.key].arn
   groups     = [each.key]
   depends_on = [ aws_iam_group.terra-group,aws_iam_group_policy.terra-group_policy ]
 }
