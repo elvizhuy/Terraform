fb713c1eaf7c083e9f21e13c947b99c20913f8ad
Nguyễn Ngọc Huy <elvizhuy@gmail.com>
2024-04-14 18:59:17 +0700

    .

--- AWS/S3/deploy.tf
+++ AWS/S3/deploy.tf
@@ -1,6 +1,12 @@
 resource "aws_s3_bucket" "development" {
-  bucket = "dev-9890"
-  tags = {
+    bucket = "dev-9890"
+    tags = {
     Description = "devops"
-  }
+    }
+}
+
+resource "aws_s3_bucket_object" "dev" {
+    content = "/home/huynn/devops.txt"
+    key = "devops.txt"
+    bucket = aws_s3_bucket.development.id
 }
