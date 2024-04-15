aed7854f2ec9127baed23066969c327c5764c2ec
Nguyen Ngoc Huy <elvihuy@gmail.com>
2024-04-15 14:07:46 +0700

    .

--- AWS/S3/deploy.tf
+++ AWS/S3/deploy.tf
@@ -15,6 +15,7 @@ resource "aws_s3_bucket_policy" "allow_access_from_another_account" {
   bucket = aws_s3_bucket.development.id
   policy = data.aws_iam_policy_document.allow_access_from_another_account.json
 }
+
 data "aws_iam_group" "terra-group" {
     group_name = "terra-group"
 }
