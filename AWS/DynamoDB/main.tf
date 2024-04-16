ffb6cb20cdb7263bbd3a0f1aa2a8eb5698f7d3d2
Nguyen Ngoc Huy <elvihuy@gmail.com>
2024-04-16 08:33:04 +0700

    .

--- AWS/DynamoDB/main.tf
+++ AWS/DynamoDB/main.tf
@@ -1,4 +1,9 @@
-resource "local_file" "test" {
-    filename = "/home/huynn/devops.txt"
-    content = "Hello"
+resource "aws_dynamodb_table" "state-locking" {
+    name = "state-locking"
+    billing_mod = "PAY_PER_REQUEST"
+    hash_key = "LockID"
+    attribute {
+        name = "LockID"
+        type = "S"
+    }
 }
--- AWS/STATE/main.tf
+++ AWS/STATE/main.tf
@@ -3,12 +3,3 @@ resource "local_file" "test" {
     content = "Hello"
 }
 
-resource "aws_dynamodb_table" "state-locking" {
-    name = "state-locking"
-    billing_mod = "PAY_PER_REQUEST"
-    hash_key = "LockID"
-    attribute {
-        name = "LockID"
-        type = "S"
-    }
-}
