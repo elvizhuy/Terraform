78dce02052418fd5d49d32c8cec23c07e7906208
Nguyen Ngoc Huy <elvihuy@gmail.com>
2024-04-16 08:30:59 +0700

    .

--- AWS/STATE/terraform.tf
+++ AWS/STATE/terraform.tf
@@ -4,5 +4,6 @@ terraform {
         key = "devops/terraform.tfstate"
         region = "ap-southeast-1"
         dynamodb_table = "state-locking"
+        depends_on = [aws_dynamodb_table.state-locking]
     }
 }
