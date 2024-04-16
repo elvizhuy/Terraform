a1bf288fea1cf3fc67b05083370e94d86e16c535
Nguyen Ngoc Huy <elvihuy@gmail.com>
2024-04-16 11:58:32 +0700

    .

--- AWS/EC2/main.tf
+++ AWS/EC2/main.tf
@@ -5,11 +5,34 @@ resource "aws_instance" "webserver" {
         name = "webserver"
         Description = "Web Server for Ubuntu"
     }
-    user_data = <<- EOF
-                #! /bin/bash
-                apt update
-                apt install -y nginx
-                systemctl start nginx.service
-                systemctl enable nginx.service
-                EOF
+
+    provisioner "remote-exec" {
+        inline = [ "sudo apt update",
+                   "sudo apt install -y nginx",
+                   "sudo systemctl enable nginx.service",
+                   "sudo systemctl start nginx.service"
+                 ]
+    }
+
+    key_name = aws_key_pair.web.id
+    vpc_security_groups_ids = [ aws_security_group.ssh-access.id ]
+}
+
+resource "aws_security_group" "ssh-access" {
+    name = "ssh-access"
+    Description = "Allow SSH access from the internet"
+    ingress {
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
+
+resource "aws_key_pair" "web" {
+    public_key = file("/home/huynn/.ssh/web.pub)
+}
+
+output publicip {
+    value = aws_instance.webserver.public_ip
 }
